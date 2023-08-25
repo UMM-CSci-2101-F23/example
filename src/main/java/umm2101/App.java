d08c0e6efd93a25497301c27994bd68724c4de82
K.K. Lamberty <lamberty@morris.umn.edu>
2023-08-25 12:45:23 -0500

    Add and exercise an instance method
    We added an instance method so that instances of App can find the square of a number.

--- src/main/java/umm2101/App.java
+++ src/main/java/umm2101/App.java
@@ -6,8 +6,15 @@ package umm2101;
  */
 public class App 
 {
+
+    public int squareANumber(int numberToSquare) {
+        return numberToSquare * numberToSquare;
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App myInstance = new App();
+        int num = 23;
+        System.out.println("The square of " + num + " is: " + myInstance.squareANumber(num));
     }
 }
